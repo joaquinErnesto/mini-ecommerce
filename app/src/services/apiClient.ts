ba11f89ce5a282c909b3bb57dde0e0d40d8b828e
Joaquin Ernesto Galan Durán <joaquinernestogalanduran@gmail.com>
2026-03-31 09:11:20 -0400

    Adding the apiClient.ts to the services

---
+++ app/src/services/apiClient.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+// Create Axios instance
+export const apiClient = axios.create({
+    baseURL: "https://fakestoreapi.com",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    timeout: 5000 // recommended
+})
+
+// Request interceptor
+apiClient.interceptors.request.use(
+    (config) => {
+        console.log("Request sent:", config)
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+// Response interceptor
+apiClient.interceptors.response.use(
+    (response) => {
+        console.log("Response recieved:", response)
+        return response
+    },
+    (error) => {
+        console.error("API Error:", error)
+        return Promise.reject(error)
+    }
+)
