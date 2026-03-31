99e796fe335c00b261e61b0ef57953e9ee6dafbd
Joaquin Ernesto Galan Durán <joaquinernestogalanduran@gmail.com>
2026-03-31 12:22:45 -0400

    Adding to the products feature its Product type in products.types.ts, its products service in products.api.ts and its useProducts hook in useProducts.ts

---
+++ app/src/features/products/services/products.api.ts
@@ -0,0 +1,28 @@
+import { apiClient } from "../../../services/apiClient";
+import type { Product } from "../types/products.types";
+
+// Get all products
+export const getProducts = async (): Promise<Product[]> => {
+    try {
+        const response = await apiClient.get("/products")
+
+        return response.data
+    } catch(error) {
+        console.error("Error fetching products: ", error)
+
+        throw error
+    }
+}
+
+// Get product by ID
+export const getProductById = async (id: number) : Promise<Product> => {
+    try {
+        const response = await apiClient.get(`/products/${id}`)
+
+        return response.data
+    } catch(error) {
+        console.error(`Error fetching product with id ${id}: `, error)
+
+        throw error
+    }
+}
