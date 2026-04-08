9cc032f069498199319170a04fdd2114fea2928b
Joaquin Ernesto Galan Durán <joaquinernestogalanduran@gmail.com>
2026-04-08 16:52:42 -0400

    Creating the stich ai folder in the project for saving in there the site page designs

--- app/src/components/layout/MainLayout.tsx
+++ app/src/components/layout/MainLayout.tsx
@@ -1,25 +1,61 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 import "./MainLayout.css"
 
 export const MainLayout = () => {
     return (
         <div className="container">
+            
             {/* Navbar */}
-            <header>
-                {/* Navbar here */}
+            <header className="navbar">
+                <div className="navbar-content">
+
+                    {/* Logo / Brand */}
+                    <Link to="/" className="logo">
+                        Mini E-Commerce
+                    </Link>
+
+                    {/* Navigation */}
+                    <nav className="nav-links">
+                        <Link to="/">
+                            Home
+                        </Link>
+                        
+                        <Link to="/">
+                            Products
+                        </Link>
+                        
+                        <Link to="/cart">
+                            Cart
+                        </Link>
+                    </nav>
+
+                </div>
             </header>
 
             {/* Main */}
             <main>
                 <div>
-                    <Outlet /> {/* 👈 THIS is where pages render */}
+                    <Outlet />
                 </div>
             </main>
 
             {/* Footer */}
-            <footer>
-                {/* Footer */}
+            <footer className="footer">
+                <div className="footer-content">
+
+                    <p>
+                        © {new Date().getFullYear()} Mini E-Commerce
+                    </p>
+
+                    <div className="footer-links">
+                        <a href="#">Privacy</a>
+                        <a href="#">Terms</a>
+                        <a href="#">Contact</a>
+                    </div>
+
+                </div>
             </footer>
+
         </div>
     )
 }
